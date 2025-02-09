2e0f49ea149910aa5035a635bc513ce72f4ee461
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-09 11:28:28 +0530

    View Component Done ✅

--- App.tsx
+++ App.tsx
@@ -1,14 +1,44 @@
-import { SafeAreaView, StyleSheet, Text} from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style = {styles.container}>
+      <View style={styles.boxContainer}>
+        <View style ={styles.redBox} />
+        <View style ={styles.blueBox} />
+        <View style ={styles.greenBox} />
+      </View>
       <Text>App</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    // flex: 1,
+    justifyContent:'center',
+    marginBottom:20,
+  },
+  boxContainer:{
+    flexDirection: 'row',
+    justifyContent:'space-around',
+  },
+  redBox:{
+    height: 50,
+    width: 50,
+    backgroundColor:'red',
+  },
+  greenBox:{
+    height: 50,
+    width: 50,
+    backgroundColor:'green',
+  },
+  blueBox:{
+    height: 50,
+    width: 50,
+    backgroundColor:'blue',
+  },
+});
