a29423f2e704a8e17281854a329cdd33da6f0949
Shubham Dhole <shubhamdhole215@gmail.com>
2025-02-09 11:41:12 +0530

    Text Component Done ✅

--- App.tsx
+++ App.tsx
@@ -4,12 +4,19 @@ import React from 'react';
 const App = () => {
   return (
     <SafeAreaView style = {styles.container}>
+      {/* View Compoent*/}
       <View style={styles.boxContainer}>
         <View style ={styles.redBox} />
         <View style ={styles.blueBox} />
         <View style ={styles.greenBox} />
       </View>
-      <Text>App</Text>
+      {/* View Compoent*/}
+      {/* Text Compoent*/}
+      <View style = {styles.textContainer}>
+        <Text style = {styles.textComponent}>My first React Native Course</Text>
+        <Text style = {styles.nestedText}>Text components can be <Text style={styles.bold}>nested</Text> as well.</Text>
+      </View>
+      {/* Text Compoent*/}
     </SafeAreaView>
   );
 };
@@ -25,11 +32,13 @@ const styles = StyleSheet.create({
   boxContainer:{
     flexDirection: 'row',
     justifyContent:'space-around',
+    padding:'2%'
   },
   redBox:{
     height: 50,
     width: 50,
     backgroundColor:'red',
+    borderRadius: '50%'
   },
   greenBox:{
     height: 50,
@@ -40,5 +49,19 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     backgroundColor:'blue',
+  },
+  textContainer:{
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  textComponent:{
+    fontSize:26,
+    padding: '2%',
+  },
+  nestedText:{
+    fontSize: 18
+  },
+  bold:{
+    fontWeight: 'bold',
   },
 });
